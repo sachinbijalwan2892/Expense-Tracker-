1d60a515181097939412bf3616058fe0910cfb51
Sachin Bijalwan <sachinbijalwanrs200@gmail.com>
2025-08-20 12:18:50 +0530

    theme toggle along with home about and contact page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { currentUser } from "@clerk/nextjs/server"
+import Guest from "@/components/Guest";
+export default async function HomePage() {
+  const user=await currentUser();
 
-
-export default function page() {
+  if(!user)
+  {
+    return <Guest/>
+  }
   return (
     <div className="text-red-400">Home page</div>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "class", // 👈 important
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
